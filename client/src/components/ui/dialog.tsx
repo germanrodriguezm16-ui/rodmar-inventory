c2b99bec68b6db2ade6fcaba09ed993419f47eab
germanrodriguezm16-ui <236298536+germanrodriguezm16-ui@users.noreply.github.com>
2026-01-18 15:13:16 -0500

    UI: modales centrados en mÃ³vil (Dialog inset)

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -38,7 +38,9 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        // Mobile: usar inset para que el modal no se “corra” (vw/vh/visual viewport) y siempre tenga bordes visibles
+        // Desktop (sm+): mantener el patrón centrado tradicional (50% + translate)
+        "fixed inset-x-3 top-3 bottom-3 z-50 grid w-auto max-w-lg gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:left-[50%] sm:top-[50%] sm:bottom-auto sm:inset-x-auto sm:w-full sm:translate-x-[-50%] sm:translate-y-[-50%] sm:rounded-lg data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
         className
       )}
       {...props}
